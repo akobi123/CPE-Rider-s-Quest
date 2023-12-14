7abce16b6c933e94ae0a418f1e365d97c84bbdba
iakob rigvava <79013242+akobi123@users.noreply.github.com>
2023-12-14 20:19:49 +0400

    Update Follow.cs

--- Follow.cs
+++ Follow.cs
@@ -2,25 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this script handles one object following another
 public class Follow : MonoBehaviour
 {
-    public GameObject followCamera;
-    //public float offsetDistance = 0.5f;
-    public Vector3 offsetRotation = new Vector3(0f, 0f, 0f);
-    public Vector3 offsetPosition = new Vector3(0f, 0f, 0.5f);
-    public bool toFollowRotation;
+    public GameObject followCamera; // object to follow
+    public Vector3 offsetRotation = new Vector3(0f, 0f, 0f); // offset rotation from the followed object
+    public Vector3 offsetPosition = new Vector3(0f, 0f, 0.5f); // offset position from the followed object
+    public bool toFollowRotation; // flag variable to check if we should follow the rotation
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //offsetPosition = followCamera.transform.forward * offsetDistance;
+        // set the rotation
         transform.rotation = (toFollowRotation ? followCamera.transform.rotation * Quaternion.Euler(offsetRotation) : Quaternion.Euler(offsetRotation));
+        // set the position
         transform.position = followCamera.transform.position + offsetPosition;
     }
 }
