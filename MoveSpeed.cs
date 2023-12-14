c9c27d70a74ec166789295f648755454251cb263
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-12-14 20:58:35 +0400

    Update MoveSpeed.cs

--- MoveSpeed.cs
+++ MoveSpeed.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this script stores speed of the objects in the game
 public class MoveSpeed : MonoBehaviour
 {
     public static float moveSpeed = 3f;
@@ -9,6 +10,7 @@ public class MoveSpeed : MonoBehaviour
 
     private void Update()
     {
+        // increase movespeed  
         moveSpeed += speedIncreaseRate * Time.deltaTime;
     }
 }
