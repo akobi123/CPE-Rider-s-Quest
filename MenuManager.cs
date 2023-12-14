8b8256fffb7ff573510b2c971612e9a3e9b339b6
iakob rigvava <79013242+akobi123@users.noreply.github.com>
2023-12-14 20:36:42 +0400

    Update MenuManager.cs

--- MenuManager.cs
+++ MenuManager.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this script handles enabling and disabling an object
 public class MenuManager : MonoBehaviour
 {
+    // enable object
     public void open(GameObject menu)
     {
         menu.SetActive( true );
     }
 
+    // disable object
     public void close(GameObject menu)
     {
         menu.SetActive( false );
