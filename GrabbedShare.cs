acda8921de9e9133e559dc90a8f850b851b0960b
iakob rigvava <79013242+akobi123@users.noreply.github.com>
2023-12-14 20:34:11 +0400

    Update GrabbedShare.cs

--- GrabbedShare.cs
+++ GrabbedShare.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// script to save the state of the string
+// grabbed or not grabbed
 public class GrabbedShare : MonoBehaviour
 {
     public static bool grabbed = false;
