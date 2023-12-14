131a72f42b6ab27a32a7d3eddb81339f0d3af26a
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-12-14 21:17:19 +0400

    Update ScoreDisplay.cs

--- ScoreDisplay.cs
+++ ScoreDisplay.cs
@@ -6,35 +6,43 @@ using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
+// This script displays score and time on canvas, also keeps track of score.
 public class ScoreDisplay : MonoBehaviour
 {
-    public Text scoreText;
-    public static ScoreDisplay instance;
-    int score = 0;
-    public Text timeText;
-    float starttime;
-    public float time;
+    public Text scoreText; // drag and drop text object where the score should show
+    public Text timeText; // drag and drop text object where the time should show
+    public static ScoreDisplay instance; // instance of this class
+
+    private float time;
+    private float starttime;
+    private int score = 0;
 
     // Start is called before the first frame update
     private void Awake()
     {
+        //create an instance so other scripts can access this one
         instance = this;
     }
     void Start()
     {
+        // Display score
         scoreText.text = "Score: " + score.ToString();
+        // initialize stattime
         starttime = Time.time;
     }
     private void Update()
     {
+        // update time
         time = (Time.time - starttime);
+        //display trime with 2 decimal precision
         timeText.text = "Time: " + time.ToString("n2"); //set precison to 2 decimal plave
     }
 
-    // Update is called once per frame
     public void addScore(int i)
     {
-        score += i * (int)Math.Round(1 + time * 0.01f); 
+        //increse score, using linear time dependent equation
+        score += i * (int)Math.Round(1 + time * 0.02f); 
+        //display score
         scoreText.text = "Score: " + score.ToString();
     }
 }
