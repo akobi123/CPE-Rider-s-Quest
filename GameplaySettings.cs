a7a36957d3062e53bd82abab683ff403b74cf2f4
iakob rigvava <79013242+akobi123@users.noreply.github.com>
2023-12-14 20:33:08 +0400

    Update GameplaySettings.cs

--- GameplaySettings.cs
+++ GameplaySettings.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this script handles saving the gameplay settings
 public class GameplaySettings : MonoBehaviour
 {
+    // static variables for the settings to be accessable from other scripts
     public static bool IsEasy = true;
     public static bool IsRightHanded = true;
     public static bool IsBobbing = true;
 
+    // functions to change each setting
     public void changeDifficulty()
     {
         IsEasy = !IsEasy;
