8e475f037408f7860f5658a839dc19d4ef50759b
iakob rigvava <79013242+akobi123@users.noreply.github.com>
2023-12-14 20:42:38 +0400

    Update StartControll.cs

--- StartControll.cs
+++ StartControll.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this scene handles changing the button on the main menu
+// based on the biome setting
 public class StartControll : MonoBehaviour
 {
+    // the buttons
     public GameObject forest;
     public GameObject desert;
+
+    // function to change the buttons
     public void changeBiome()
     {
+        // if forest is picked set its button as active
+        // and the other one as inactive
         if (GameplaySettings.IsBobbing == true)
         {
             forest.SetActive(true);
             desert.SetActive(false);
         }
+        // if desert is picked set its button as active
+        // and the other one as inactive
         else
         {
             forest.SetActive(false);
