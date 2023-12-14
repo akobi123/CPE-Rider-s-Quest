dd0dd387930de453af5804c9f169df817db968b2
iakob rigvava <79013242+akobi123@users.noreply.github.com>
2023-12-14 20:46:43 +0400

    Update quit.cs

--- quit.cs
+++ quit.cs
@@ -2,24 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this script handles quitting the application
 public class quit : MonoBehaviour
 {
-    public int smth;
-
+    // function to quit the application
     public void quitProgram()
     {
         Application.Quit();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
