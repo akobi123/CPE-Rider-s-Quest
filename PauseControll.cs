93214d1cfc9773a821023c01ad25b81f61aaf4ca
iakob rigvava <79013242+akobi123@users.noreply.github.com>
2023-12-14 20:37:46 +0400

    Update PauseControll.cs

--- PauseControll.cs
+++ PauseControll.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this script handles pausing and resuming the game
 public class PauseControll : MonoBehaviour
 {
+    // pause the game
     public void pause()
     {
         Time.timeScale = 0f;
     }
 
+    // resume the game
     public void resume()
     {
         Time.timeScale = 1f;
